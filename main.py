24bc9e6d15d10444c3449de2c28c967fa69ac985
mehrniaamouei <mehrniaamouei@gmail.com>
2025-10-16 00:28:58 +0330

    refactor: rewrite main for test

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ if __name__ == "__main__":
         print("Project made", p1)
         manager.edit_project("first project", "first project", "hjm")
         
-        p2 = manager.create_project("first project", "sec rep")
+        p2 = manager.create_project("first project", "sec rep.")
     except Exception as e:
         print("error", e)
     
