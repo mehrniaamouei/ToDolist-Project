f8e46a4281c525278e8f2c92c4ee5dd38028ce4d
mehrniaamouei <mehrniaamouei@gmail.com>
2025-10-15 20:21:50 +0330

    style: correct the output

--- todolist/project.py
+++ todolist/project.py
@@ -7,14 +7,14 @@ MAX_NUMBER_OF_PROJECT = int(os.getenv("MAX_NUMBER_OF_PROJECT", 5))
 class Project:
     def __init__(self, name: str, description: str):
         if len(name) > 30:
-            raise ValueError("Name of projects cannot be more than 30 characters")
+            raise ValueError("Name of projects cannot be more than 30 characters.")
         if len(description) > 150:
-            raise ValueError("Description cannot be more than 150 characters")
+            raise ValueError("Description cannot be more than 150 characters.")
         self.name = name
         self.description = description
 
     def __repr__(self):
-        return f"<Project: {self.name}>"
+        return f"<<Project: {self.name}>>"
 
 class ProjectManager:
     def __init__(self):
@@ -22,9 +22,9 @@ class ProjectManager:
 
     def create_project(self, name: str, description: str):
         if len(self.projects) >= MAX_NUMBER_OF_PROJECT:
-            raise Exception("Number of projects out of range")
+            raise Exception("Number of projects out of range.")
         if any(p.name == name for p in self.projects):
-            raise Exception("There is a project with this name")
+            raise Exception("There is a project with this name.")
         project = Project(name, description)
         self.projects.append(project)
         return project
@@ -32,11 +32,11 @@ class ProjectManager:
     def edit_project(self, old_name: str, new_name: str, new_description: str):
         project = next((p for p in self.projects if p.name == old_name), None)
         if not project:
-            raise Exception(f"No project with '{old_name}' name was found")
+            raise Exception(f"No project with '{old_name}' name was found.")
         if len(new_name) > 30:
-            raise ValueError("Name of projects cannot be more than 30 characters")
+            raise ValueError("Name of projects cannot be more than 30 characters.")
         if len(new_description) > 150:
-            raise ValueError("Description cannot be more than 150 characters")
+            raise ValueError("Description cannot be more than 150 characters.")
         
         if any(p.name == new_name and p != project for p in self.projects):
             raise Exception("Choose another name.")
@@ -47,3 +47,4 @@ class ProjectManager:
      
     def list_projects(self):
         return self.projects
+    
